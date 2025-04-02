1b0392b80bb8d7495f26f49e1ad540695b945f38
GonzaloPurita <gpurita@frba.utn.edu.ar>
2025-04-02 19:24:16 -0300

    Falta hacer MCM y MCD

--- resueltosGuiaEjs.hs
+++ resueltosGuiaEjs.hs
@@ -82,3 +82,50 @@ cuadradoPerfectoMayorQue numero
 
 cantidadCuadradosPerfectos :: Int -> Int -> Int
 cantidadCuadradosPerfectos desde hasta = length (filter esCuadradoPerfecto [desde..hasta])
+
+
+-- Minimo comun multiplo
+
+
+
+-- maximo comun divisor
+
+
+
+-- DISPERSION --
+
+dispersion :: Int -> Int -> Int -> Int
+dispersion med1 med2 med3 = mayorDeTres med1 med2 med3 - menorDeTres med1 med2 med3
+
+dispersionChica :: Int -> Bool
+dispersionChica dispersion = dispersion < 30
+
+dispersionGrande :: Int -> Bool
+dispersionGrande dispersion = dispersion > 100
+
+menorDeTres :: Ord a => a -> a -> a -> a
+menorDeTres elem1 elem2 elem3
+                    | elem1 <= elem2 = min elem1 elem3
+                    | otherwise = min elem2 elem3
+
+diasParejos :: Int -> Int -> Int -> Bool
+diasParejos med1 med2 med3 = dispersionChica (dispersion med1 med2 med3)
+
+diasLocos :: Int -> Int -> Int -> Bool
+diasLocos med1 med2 med3 = dispersionGrande (dispersion med1 med2 med3)
+
+diasNormales :: Int -> Int -> Int -> Bool
+diasNormales med1 med2 med3 = not (diasParejos med1 med2 med3) && not (diasLocos med1 med2 med3)
+
+
+-- PINOS --
+
+pesoPino :: Int -> Int
+pesoPino altura = min altura 3 * 100 * 3 + max (altura - 3) 0 * 100 * 2
+
+esPesoUtil :: Int -> Bool
+esPesoUtil peso = peso `elem` [400..1000]   -- peso >= 400 && peso <= 1000
+
+sirvePino :: Int -> Bool
+sirvePino altura = esPesoUtil (pesoPino altura)
+
